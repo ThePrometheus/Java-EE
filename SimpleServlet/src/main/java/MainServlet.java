dcc66b57b9d68bcaa9010c936e5a363aec22ede1
ThePrometheus <nazartkach3@gmail.com>
2017-01-22 10:20:57 +0200

    Added servlet

---
+++ SimpleServlet/src/main/java/MainServlet.java
@@ -0,0 +1,41 @@
+/**
+ * Created by nazar on 22.01.17.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+
+    private final static String  _name = "NAZAR";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+
+        req.getRequestDispatcher("ask.jsp").forward(req, resp);
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+        String username = req.getParameter("username");
+        if(username.equals(_name)){
+            String htmlResponse = "<html>";
+            htmlResponse += "<h2>Hello out admin: " + username + "<br/>";
+            htmlResponse += "</html>";
+        }else{
+            String htmlResponse = "<html>";
+            htmlResponse += "<h2>incorrect name : " + username + "<br/>";
+            htmlResponse += "</html>";
+
+        }
+
+
+
+    }
+}
+
---
+++ SimpleServlet/src/main/webapp/Ask.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<html>
+<head>
+    <title> Ask  your name JSP </title>
+</head>
+<body>
+<form action="HelloServlet"> Please enter a name <br>
+    <input type="text" name="username"size="20px">
+    <input type="submit" value="Login"> </form>
+</body>
+</html>
